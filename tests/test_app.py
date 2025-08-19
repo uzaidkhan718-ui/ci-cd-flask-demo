ac7712875a2ad78a5fb19466b2c29836e7dc52c3
uzaidkhan718-ui <uzaidkhan718@gmail.com>
2025-08-20 00:36:50 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 def test_home():
     client = app.test_client()
-    res = client.get("/")
-    assert res.status_code == 200
-    assert b"Welcome to My DevOps CI/CD Project" in res.data
+    resp = client.get("/")
+    assert resp.status_code == 200
 
 def test_status():
     client = app.test_client()
-    res = client.get("/status")
-    assert res.status_code == 200
-    data = res.get_json()
-    assert data["ok"] is True
+    resp = client.get("/status")
+    assert resp.json["ok"] == True
+
